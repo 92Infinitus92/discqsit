4dae08b424c83775111ddd925060f2c4d3cb4ccd
Benjaminj Jones <benjaminj@dev-it.info>
2024-03-21 10:31:12 +0200

    proceeding with the create topic form

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,3 +1,5 @@
 "use server";
 
-export async function createPost() {}
+export async function createPost() {
+  //TODO: revalidate the topicshowpage after a post is created
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,14 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions/actions";
-import { auth } from "../auth";
-import Profile from "../components/profile";
+import TopicCreateForm from "../components/topics/topic-create-form";
 
-export default async function Home() {
-  const session = await auth();
-
-  // if (session?.user) {
-  //   return <div>Hi {session.user.name}</div>;
-  // }
-
+export default function Home() {
   return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-
-      {session?.user ? (
-        <div>Hi {session.user.name}</div>
-      ) : (
-        <div>Not signed in</div>
-      )}
-      <Profile />
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
     </div>
   );
 }
